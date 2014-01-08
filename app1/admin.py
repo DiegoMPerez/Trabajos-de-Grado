ebc7a884f22e76476029843fe53166d94bdc3a0b
DiegoMPerez <dmperez@utn.edu.ec>
2014-01-08 10:07:28 -0500

    Registro completo de modelos

--- app1/admin.py
+++ app1/admin.py
@@ -5,3 +5,12 @@ from app1.models import *
 admin.site.register(Estudiante)
 admin.site.register(Director)
 admin.site.register(Docente)
+admin.site.register(TrabajoDeGrado)
+admin.site.register(Jurados)
+admin.site.register(Defensa)
+admin.site.register(Versionamiento)
+admin.site.register(Desarrollodefases)
+admin.site.register(Fasesdesarrollo)
+admin.site.register(CorreccionDocente)
+admin.site.register(CorreccionDirector)
+
