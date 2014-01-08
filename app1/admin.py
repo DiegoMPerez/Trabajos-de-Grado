a0b16b153a4adb2168d29c04b6751615313e0deb
DiegoMPerez <dmperez@utn.edu.ec>
2014-01-08 09:56:42 -0500

    registro de modelos

--- app1/admin.py
+++ app1/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from app1.models import Estudiante
-
+from app1.models import *
 # Register your models here.
 
 admin.site.register(Estudiante)
+admin.site.register(Director)
+admin.site.register(Docente)
